a9d47f6c824a691c0bfbb2257e6110d7339fb16f
Anirudh Swaminathan <aniswami97@gmail.com>
2019-11-29 19:36:39 -0800

    Compute the eigen faces
    Eigen faces have been computed for the images

--- src/project.py
+++ src/project.py
@@ -10,6 +10,9 @@ import random
 # matrices
 import numpy as np
 
+# eigen value computation
+from scipy.linalg import eigh
+
 
 class EigenFaces(object):
     """
@@ -26,6 +29,7 @@ class EigenFaces(object):
         self.mean_img = None
         self.dataset = []
         self.image_size = None
+        self.eigen_faces = None
 
     def load_data(self):
         """Function to load the data"""
@@ -38,6 +42,8 @@ class EigenFaces(object):
         self.dataset = np.array(self.dataset)
         self.image_size = (self.dataset.shape[1], self.dataset.shape[2])
         self.dataset = self.dataset.reshape(self.dataset.shape[0], self.dataset.shape[1] * self.dataset.shape[2])
+        # 31266 * 190 - dataset shape
+        self.dataset = np.transpose(self.dataset)
 
     def show_image(self, ind, mode="neutral"):
         """Function to show a random image"""
@@ -56,15 +62,43 @@ class EigenFaces(object):
 
     def mean_image(self):
         """Calculate the mean image of a given neutral dataset"""
-        num_imgs = self.dataset.shape[0]
+        num_imgs = self.dataset.shape[1]
         # print(self.dataset.shape)
-        self.mean_img = np.matmul(np.transpose(self.dataset), np.ones((num_imgs, 1))) / num_imgs
-        self.mean_img = np.transpose(self.mean_img)
+        self.mean_img = np.matmul(self.dataset, np.ones((num_imgs, 1))) / num_imgs
+        # 31266 * 1 - mean_img
+        self.mean_img = self.mean_img
         print(self.mean_img.shape)
 
     def pca(self):
+        # computes the mean image of the dataset
         self.mean_image()
+
+        # computes image - mean for all images
+        # 31266 * 180
         self.mean_offset = self.dataset - self.mean_img
+
+        # 180*180
+        mod_cov = np.matmul(np.transpose(self.mean_offset), self.mean_offset)
+        print(mod_cov.shape)
+
+        # check if this is a symmetric matrix
+        print(np.allclose(mod_cov, np.transpose(mod_cov)))
+
+        # compute the eigen values and the corresponding eigenvectors in ascending order
+        # 190 eigen values
+        # 190 * 190 eigen vectors
+        # ith column - corresponding to the ith eigen vector
+        eig_vals, mod_eig_vecs = eigh(mod_cov)
+        print(eig_vals.shape, mod_eig_vecs.shape)
+
+        # 31266 * 190 - eigen vectors of the original data
+        eig_vecs = np.matmul(self.mean_offset, mod_eig_vecs)
+
+        # normalize the eigen vectors now
+        # 31266 * 190 normalized eigen vectors
+        norm_cnst = np.sum(np.square(eig_vecs), 0)
+        self.eigen_faces = np.divide(eig_vecs, norm_cnst)
+        print(self.eigen_faces.shape)
 
 
 def main():
@@ -78,6 +112,10 @@ def main():
     efc.pca()
     plot_mean_img = efc.mean_img.reshape(efc.image_size[0], efc.image_size[1])
     efc.display_image(plot_mean_img)
+    for j in range(5):
+        img = efc.eigen_faces[:, 189-j]
+        plot_face = img.reshape(efc.image_size[0], efc.image_size[1])
+        efc.display_image(plot_face)
 
 
 if __name__ == "__main__":
